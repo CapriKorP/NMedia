21c24a850b9903d1a7e578b814628f826fadb8a5
Pavel Kornienkov <122346047+CapriKorP@users.noreply.github.com>
2024-04-20 01:01:48 +0300

    Intents task 1, no clean

--- app/src/main/java/ru/netology/nmedia/activity/NewPostActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostActivity.kt
@@ -3,11 +3,14 @@ package ru.netology.nmedia.activity
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.content.Intent.*
+import android.content.Intent.EXTRA_TEXT
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.viewmodel.compose.viewModel
 import ru.netology.nmedia.databinding.ActivityNewPostBinding
+import ru.netology.nmedia.viewmodel.PostViewModel
 
 
 class NewPostActivity : AppCompatActivity() {
@@ -15,6 +18,8 @@ class NewPostActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityNewPostBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val viewModel: PostViewModel by viewModels()
 
         binding.content.requestFocus()
         binding.content.setText(intent?.getStringExtra(Intent.EXTRA_TEXT))
@@ -29,19 +34,20 @@ class NewPostActivity : AppCompatActivity() {
         }
 
         binding.back.setOnClickListener{
+            viewModel.cancelEdit()
             finish()
         }
     }
 }
 
-object NewPostContract: ActivityResultContract<String?,String?>(){
-    override fun createIntent(context: Context, input: String?) = Intent(context,NewPostActivity::class.java).putExtra(Intent.EXTRA_TEXT, input)
+object NewPostContract: ActivityResultContract<String?,String?>() {
+    override fun createIntent(context: Context, input: String?) =
+        Intent(context, NewPostActivity::class.java).putExtra(Intent.EXTRA_TEXT, input)
 
     override fun parseResult(resultCode: Int, intent: Intent?): String? =
-        if(resultCode == Activity.RESULT_OK) {
+        if (resultCode == Activity.RESULT_OK) {
             intent?.getStringExtra(Intent.EXTRA_TEXT)
         } else {
             null
         }
-
 }
